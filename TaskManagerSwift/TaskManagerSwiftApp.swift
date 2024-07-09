aca3e1e6435c34d9ce01d41d7957ae8d087ca67a
Shameer24 <mohamedshameer2406@gmail.com>
2024-07-08 20:20:12 -0400

    Initial Commit

---
+++ TaskManagerSwift/TaskManagerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerSwiftApp.swift
+//  TaskManagerSwift
+//
+//  Created by Mohamed Shameer on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
